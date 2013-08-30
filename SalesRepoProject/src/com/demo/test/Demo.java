403274c3cb4cff6c06bdd6d1203b841722b6d333
yogeshrkolhe <yogeshr.kolhe@yahoo.com>
2013-08-30 14:08:27 -0400

    Initial Push

---
+++ SalesRepoProject/src/com/demo/test/Demo.java
@@ -0,0 +1,14 @@
+package com.demo.test;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is demo.java");
+
+	}
+
+}
---
+++ SalesRepoProject/src/com/demo/test/Test.java
@@ -0,0 +1,13 @@
+package com.demo.test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test.java");
+	}
+
+}
