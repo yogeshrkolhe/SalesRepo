d25ed43c158acaa4eb1caeee465734a6a0080ae9
yogeshrkolhe <yogeshr.kolhe@yahoo.com>
2013-08-30 22:05:19 -0400

    commit of test.java

--- SalesRepoProject/src/com/demo/test/Test.java
+++ SalesRepoProject/src/com/demo/test/Test.java
@@ -8,6 +8,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is test.java");
+		
+		System.out.println("This is the second line in test.java");
 	}
 
 }
