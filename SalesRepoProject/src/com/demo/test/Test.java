bed15a5217b5fbc19a8d231a6c02083077f409bd
kadamvi4 <vishal.kadam@cognizant.com>
2013-09-03 17:11:58 +0530

    coomit for merge by vishal

--- SalesRepoProject/src/com/demo/test/Test.java
+++ SalesRepoProject/src/com/demo/test/Test.java
@@ -9,13 +9,16 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("This is test.java");
 
-		System.out.println("This is the second line in test.java added by Vishal");
+		System.out.println("This is the second line in test.java  ");
 
 		System.out.println("This is the third line in test.java");
 
 		System.out.println("This is fourthline by yogesh during tansition");
-		
+
 		System.out.println("This is the 5th line added by yogesh to test merge");
+
+		System.out.println("Thi is a line added by vishal.");
+
 	}
 
 }
