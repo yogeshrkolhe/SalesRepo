36c451d35d43c7d9c8b2d9733188834d25b062b1
kadamvi4 <vishal.kadam@cognizant.com>
2013-09-03 16:38:36 +0530

    commited by vishal

--- SalesRepoProject/src/com/demo/test/Demo.java
+++ SalesRepoProject/src/com/demo/test/Demo.java
@@ -7,14 +7,14 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is demo.java");
+		System.out.println("This is demo.java by Vishal");
 
 		System.out.println("Code modified by YRK");
-		
+
 		System.out.println("COde modified by VK");
-		
+
 		System.out.println("Checking merge and conflict by VK");
-		
+
 		System.out.println("Checking merge and conflict by YRK");
 	}
 
