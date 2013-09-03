573bd7d66f134f247fea75fbe59cd5f2372fbd8e
kadamvi4 <vishal.kadam@cognizant.com>
2013-09-03 16:59:48 +0530

    Commited by Vishal 2

--- SalesRepoProject/src/com/demo/test/Test.java
+++ SalesRepoProject/src/com/demo/test/Test.java
@@ -8,11 +8,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is test.java");
-		
-		System.out.println("This is the second line in test.java");
-		
+
+		System.out.println("This is the second line in test.java added by Vishal");
+
 		System.out.println("This is the third line in test.java");
-		
+
 		System.out.println("This is fourthline by yogesh during tansition");
 	}
 
